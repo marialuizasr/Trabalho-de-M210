bc1460ce437e2f623337c1e33456e6d9b17ada27
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-22 09:35:40 -0300

    Add Grapher class with plot_2d and plot_3d methods

---
+++ src/grapher.py
@@ -0,0 +1,10 @@
+
+class Grapher:
+    def __init__(self):
+        pass
+    
+    def plot_2d(self, restrictions, target_function):
+        pass
+
+    def plot_3d(self, restrictions, target_function):
+        pass
