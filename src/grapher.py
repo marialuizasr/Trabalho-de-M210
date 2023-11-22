f0e560ec5f8f0b0f054993ebb34d788ef3dfd882
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-22 10:39:35 -0300

    Added plot_3d method 🚧🚧🚧

--- src/grapher.py
+++ src/grapher.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 class Grapher:
 
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        pass
 
-    def plot_2d(self, restrictions, target_function=None):
+    def plot_2d(self, restrictions, target_function=None, config=None):
 
         # Transform restrictions into a list of coordinates where it crosses the x and y axis on the graph
         restrictions = [{'x':(restriction['y']/restriction['x1'], 0), 
@@ -30,28 +30,75 @@ class Grapher:
         # Plots the restrictions
         for restriction in restrictions:
             color = 'red' if restriction['type'] == 'less' else 'blue' if restriction['type'] == 'equal' else 'green'
-            ax.plot(restriction['x'], restriction['y'], color='black', alpha=0.8, linewidth=0.5)
-            ax.fill_between(restriction['x'], restriction['y'], color=color, alpha=0.1)
+            ax.plot(restriction['x'], restriction['y'], color='black', alpha=0.8, linewidth=0.8)
+            ax.fill_between(restriction['x'], restriction['y'], color=color, alpha=0.05)
 
         # Plots the target function
         # ax.plot(target_function[0], target_function[1], color='black')
 
         # Set labels from config
         ax.set_title('Método Gráfico')
-        ax.set_xlabel(self.config['x'])
-        ax.set_ylabel(self.config['y'])
+        ax.set_xlabel(config['x'])
+        ax.set_ylabel(config['y'])
 
         # Shows the graph
         plt.show()
 
 
-    def plot_3d(self, restrictions, target_function):
-        pass
+    def plot_3d(self, restrictions, target_function=None, config=None):
+        
+        # Transform restrictions into a list of coordinates where it crosses the x and y axis on the graph
+        restrictions = [{'x':(restriction['y']/restriction['x1'], 0, 0), 
+                         'y':(0, restriction['y']/restriction['x2'], 0), 
+                         'z':(0, 0, restriction['y']/restriction['x3']),
+                         'type':restriction['type']} for restriction in restrictions]
+        
+        # Calculate min and max values from restrictions
+        max_x = max(restriction['x'][0] for restriction in restrictions)
+        max_y = max(restriction['y'][1] for restriction in restrictions)
+        max_z = max(restriction['z'][2] for restriction in restrictions)
+        diff_x = max_x*0.1
+        diff_y = max_y*0.1
+        diff_z = max_z*0.1
+
+        # Creates the graph
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_xlim(0 - diff_x, max_x + diff_x)
+        ax.set_ylim(0 - diff_y, max_y + diff_y)
+        ax.set_zlim(0 - diff_z, max_z + diff_z)
+        ax.grid(True, which='both')
+
+        # Plots the restrictions as planes on the graph
+        for restriction in restrictions:
+            color = 'red' if restriction['type'] == 'less' else 'blue' if restriction['type'] == 'equal' else 'green'
+            ax.plot_trisurf(restriction['x'], restriction['y'], restriction['z'], color=color, alpha=0.333)
+
+            # Create a grid for the base surface
+            x = np.linspace(min(restriction['x']), max(restriction['x']), len(restriction['x']))
+            y = np.linspace(min(restriction['y']), max(restriction['y']), len(restriction['y']))
+            X, Y = np.meshgrid(x, y)
+            Z = np.zeros_like(X)
+
+            # Plot the base surface
+            ax.plot_surface(X, Y, Z, color=color, alpha=0.05)
+
+        # Set labels from config
+        ax.set_title('Método Gráfico')
+        ax.set_xlabel(config['x'])
+        ax.set_ylabel(config['y'])
+        ax.set_zlabel(config['z'])
+
+        # Shows the graph
+        plt.show()
+
 
 if __name__ == '__main__':
 
-    restrictions = [{'x1':2, 'x2':4, 'y':16, 'type':'more'}, {'x1':3, 'x2':2, 'y':12, 'type':'less'}]
-    config = {'x':'nome do eixo X', 'y':'nome do eixo Y'}
+    restrictions_2d = [{'x1':2, 'x2':4, 'y':16, 'type':'less'}, {'x1':3, 'x2':2, 'y':12, 'type':'less'}]
+    restrictions_3d = [{'x1':1, 'x2':2, 'x3':3, 'y':30, 'type':'less'}, {'x1':2, 'x2':1, 'x3':1, 'y':20, 'type':'more'}, {'x1':1, 'x2':1, 'x3':2, 'y':24, 'type':'less'}]
+    config = {'x':'nome do eixo X', 'y':'nome do eixo Y', 'z':'nome do eixo Z'}
 
-    grapher = Grapher(config)
-    grapher.plot_2d(restrictions)
+    grapher = Grapher()
+    # grapher.plot_2d(restrictions_2d, config=config)
+    grapher.plot_3d(restrictions_3d, config=config)
